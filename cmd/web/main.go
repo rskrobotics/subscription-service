a7664c6b2a0269134de183f5edea835458aab2d6
Krzysztof <rskrobotics@gmail.com>
2024-10-31 00:11:51 +0100

    Initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,20 @@
+package main
+
+const webPort="80"
+
+func main(){
+	// connect to db
+
+	// create sessions
+
+	//create some channels
+
+	// create waitgroup
+
+	//set application config
+
+	//set up mail
+
+	//listen for web connections
+
+}
